d3f218bcdb1db1c8b9f268410a25a2bb74d8ffb8
Vignesh N 2415 <vigneshn051995.com>
2020-02-25 10:21:31 +0530

    modified port

--- src/main/java/com/chainsys/busticketapp/BusticketappApplication.java
+++ src/main/java/com/chainsys/busticketapp/BusticketappApplication.java
@@ -2,8 +2,12 @@ package com.chainsys.busticketapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude=DataSourceAutoConfiguration.class)
+@ServletComponentScan("com.chainsys")
 public class BusticketappApplication {
 
 	public static void main(String[] args) {
